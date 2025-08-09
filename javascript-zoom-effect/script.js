9b5b3da3741a7ef55f64e7691e13fa215a42bbd9
jonkostokito <153649553+jonkostokito@users.noreply.github.com>
2025-08-09 19:01:06 +0200

    Update script.js

--- javascript-zoom-effect/script.js
+++ javascript-zoom-effect/script.js
@@ -10,3 +10,15 @@ container.addEventListener("mousemove", (e) => {
     img.style.transform = "scale(2)";
 });
 
+const container = document.getElementById("container");
+const img = document.querySelector("img");
+
+container.addEventListener("mousemove", (e) => {
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+    console.log(x, y);
+
+    img.style.transformOrigin = `${x}px ${y}px`;
+    img.style.transform = "scale(2)";
+});
+
